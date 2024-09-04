32e622a947bb4e8f7c9e565a38f4ff39cb04ce3a
Siri <xd42069xd69420@gmail.com>
2024-09-04 13:32:51 +1000

    Ran cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -2,19 +2,28 @@
 #![deny(unsafe_code)]
 #![deny(unstable_features)]
 
-//! A simple crate for generating GraphViz dot directed trees, 
+//! A simple crate for generating GraphViz dot directed trees,
 //!     based on an arbitrary tree structure.
 //! A tree can be any struct that implements:
 //!     [std::string::ToString], [std::hash::Hash] and [TreeVizNode].
 //! Currently, this crate does not support recursive elements within a tree.
 
-use std::{collections::HashSet, hash::{DefaultHasher, Hash, Hasher}, string::ToString};
+use std::{
+    collections::HashSet,
+    hash::{DefaultHasher, Hash, Hasher},
+    string::ToString,
+};
 
 /// A trait that represents a node in an arbitrary tree structure.
 /// To use this with [draw_nodes], you also need to implement [std::hash::Hash].
-pub trait TreeVizNode where Self: ToString {
+pub trait TreeVizNode
+where
+    Self: ToString,
+{
     /// Returns a vector containing the sub-nodes that are children of this node.
-    fn children(&self) -> Vec<Self> where Self: Sized;  
+    fn children(&self) -> Vec<Self>
+    where
+        Self: Sized;
 }
 
 /// Returns a visualisation of the tree with the root node `node` in the GraphViz DOT format, as a
@@ -22,15 +31,27 @@ pub trait TreeVizNode where Self: ToString {
 /// - `graph_name` is the name of the graph, which will have spaces and non-ascii characters
 ///     removed to comply with DOT's format restrictions.
 pub fn draw_nodes<T: TreeVizNode + Hash>(graph_name: &str, node: T) -> String {
-    let graph_name: String = graph_name.to_owned().chars().filter(|c| *c != ' ' && c.is_ascii()).collect();
+    let graph_name: String = graph_name
+        .to_owned()
+        .chars()
+        .filter(|c| *c != ' ' && c.is_ascii())
+        .collect();
     let mut out = format!("digraph {} {{", graph_name);
     out = format!("{}\n{}", out, draw_node(None, node, &mut HashSet::new()));
-    out = out.lines().map(|ln| ln.trim()).filter(|ln| *ln != ";").collect();
+    out = out
+        .lines()
+        .map(|ln| ln.trim())
+        .filter(|ln| *ln != ";")
+        .collect();
     out.push('}');
     out
 }
 
-fn draw_node<T: TreeVizNode + Hash>(parent_hash: Option<u64>, node: T, hashes: &mut HashSet<u64>) -> String {
+fn draw_node<T: TreeVizNode + Hash>(
+    parent_hash: Option<u64>,
+    node: T,
+    hashes: &mut HashSet<u64>,
+) -> String {
     let mut hasher = DefaultHasher::new();
     node.hash(&mut hasher);
     let mut hash = hasher.finish();
@@ -49,5 +70,9 @@ fn draw_node<T: TreeVizNode + Hash>(parent_hash: Option<u64>, node: T, hashes: &
 }
 
 fn sanitize(s: String) -> String {
-    s.replace(|c: char| !c.is_ascii(), "").replace('"', "\\\"").chars().filter(|c| c.is_ascii()).collect()
+    s.replace(|c: char| !c.is_ascii(), "")
+        .replace('"', "\\\"")
+        .chars()
+        .filter(|c| c.is_ascii())
+        .collect()
 }
