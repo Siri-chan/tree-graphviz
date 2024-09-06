8c87170907c03cf1e541f0f1c6af9a4b7b076c2f
Siri <xd42069xd69420@gmail.com>
2024-09-06 11:54:10 +1000

    Added an async variant of the algorithm

--- README.md
+++ README.md
@@ -7,4 +7,12 @@ A tree can be any struct that implements:
 Currently, this crate does not support recursive elements within a tree.
 
 This crate aims to be dependency free, and fast.
+An optional `"async"` feature is available and provides an async variant of
+    `draw_nodes` - `draw_nodes_async`, which will recurse through a 
+    node's children concurrently.
+This introduces a dependency on `futures`, but may be quicker, especially if
+    futures is already in your dependency tree.
+
+If you want to run tests on this crate, use `cargo test --all-features`, 
+    or the async-related tests will not run.
 
--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,13 @@
 //! A simple crate for generating GraphViz dot directed trees,
 //!     based on an arbitrary tree structure.
 //! A tree can be any struct that implements:
-//!     [std::string::ToString], [std::hash::Hash] and [TreeVizNode].
+//!     [`std::string::ToString`], [`std::hash::Hash`] and [`TreeVizNode`].
 //! Currently, this crate does not support recursive elements within a tree.
+//! An optional `"async"` feature is available and provides an async variant of
+//!     `draw_nodes` - `draw_nodes_async`, which will recurse through a 
+//!     node's children concurrently.
+//! This introduces a dependency on the `futures` crate, but may be quicker, 
+//!     especially if `futures` is already in your dependency tree.
 
 use std::{
     collections::HashSet,
@@ -75,4 +80,63 @@ fn sanitize(s: String) -> String {
         .chars()
         .filter(|c| c.is_ascii())
         .collect()
+}
+
+#[cfg(test)]
+mod tests;
+
+#[cfg(feature = "async")]
+use futures::future::join_all;
+#[cfg(feature = "async")]
+use std::sync::{Arc, Mutex};
+
+/// Returns a future promising the visualisation of the tree with the root node `node` in the GraphViz DOT format, as a
+/// string.
+/// - `graph_name` is the name of the graph, which will have spaces and non-ascii characters
+///     removed to comply with DOT's format restrictions.
+#[cfg(feature = "async")]
+pub async fn draw_nodes_async<T: TreeVizNode + Hash>(graph_name: &str, node: T) -> String {
+    let graph_name: String = graph_name
+        .to_owned()
+        .chars()
+        .filter(|c| *c != ' ' && c.is_ascii())
+        .collect();
+    let mut out = format!("digraph {} {{", graph_name);
+    out = format!("{}\n{}", out, draw_node_async(None, node, Arc::new(Mutex::new(HashSet::new()))).await);
+    out = out
+        .lines()
+        .map(|ln| ln.trim())
+        .filter(|ln| *ln != ";")
+        .collect();
+    out.push('}');
+    out
+}
+
+
+#[cfg(feature = "async")]
+async fn draw_node_async<T: TreeVizNode + Hash>(
+    parent_hash: Option<u64>,
+    node: T,
+    hashes: Arc<Mutex<HashSet<u64>>>,
+) -> String {
+    let mut hasher = DefaultHasher::new();
+    node.hash(&mut hasher);
+    let mut hash = hasher.finish();
+    {
+        let mut _hashes = hashes.clone();
+        let mut hashesa = _hashes.lock().unwrap();
+        while hashesa.contains(&hash) {
+            hash += 1;
+        }
+        hashesa.insert(hash);
+    }
+    let mut out = format!("{} [label=\"{}\"];\n", hash, sanitize(node.to_string()));
+    if let Some(parent) = parent_hash {
+        out = format!("{}{} -> {};\n", out, parent, hash);
+    }
+    let promises = node.children().into_iter().map(|child| {
+        draw_node_async(Some(hash), child, hashes.clone())
+    });
+    out = format!("{}{}", out, join_all(promises).await.into_iter().map(|s| format!("{};\n", s)).collect::<String>());
+    out
 }
